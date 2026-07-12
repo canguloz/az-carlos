bf0a1bd166085bd3a4245bccdad29e36f626c039
Carlos Angulo Zegarra <carlosangulozegarra@gmail.com>
2026-07-12 09:00:14 -0500

    Revert "chore: script inicio automático live server puerto 5500"
    This reverts commit 04d234655103790eaacf20686c5f814886e158c5.

--- start-server.ps1
+++
@@ -1,4 +0,0 @@
-$port = 5500
-$process = netstat -ano | Select-String ":$port " | ForEach-Object { ($_ -split '\s+')[-1] } | Select-Object -First 1
-if ($process) { Stop-Process -Id $process -Force -ErrorAction SilentlyContinue; Start-Sleep 2 }
-Start-Process -FilePath "pythonw" -ArgumentList "-m http.server $port --directory E:\PORTAFOLIO\azconsulting" -WindowStyle Hidden
